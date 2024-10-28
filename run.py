6c02467a6460902768ee1f9b62784fd5eddad88a
Katy Ashworth <katyashworth27@gmail.com>
2024-10-28 21:34:48 +0000

    Wire up APIs and confirm access to google sheet
    Import libraries
    pandas, gspread, google-auth

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+import pandas as pd
+print(pd.__version__)
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('good_food_network')
+
+inventory =SHEET.worksheet('inventory')
+
+data = inventory.get_all_values()
+
+print(data)
