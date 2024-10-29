8f72c877c1711aca263b296a44ee041ee41654e2
Katy Ashworth <katyashworth27@gmail.com>
2024-10-29 09:08:56 +0000

    Login Program
    Add login.txt file
    Create logins function and loops
    Return plain Welcome message

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 import pandas as pd
-print(pd.__version__)
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,6 +17,55 @@ SHEET = GSPREAD_CLIENT.open('good_food_network')
 
 inventory =SHEET.worksheet('inventory')
 
-data = inventory.get_all_values()
 
-print(data)
+def read_logins():
+    with open('logins.txt', 'r') as f:
+        contents = f.readlines()
+
+        new_contents = []
+
+        for line in contents:
+            fields = line.split(',')
+            fields[1] = fields[1].rstrip()
+            new_contents.append(fields)
+
+        return new_contents
+
+
+logins = read_logins()
+
+
+def login():
+    
+    """
+    login page
+    https://www.youtube.com/watch?v=L2i6lELbNI0
+    https://github.com/rodbove/console-login-system/blob/master/main.py
+    https://docs.gspread.org/en/latest/
+     """
+
+   
+    ask_username = str(input('Please enter your full name:  '))
+    ask_password =str(input('Please enter you password:  '))
+
+    
+ 
+    logged_in = False
+
+    for line in logins:
+        if line[0] == ask_username and line[1] == ask_password:
+            print('Logged in successfully...')
+            main()
+
+    print('Username / Password is incorrect')
+    login()
+
+def main():
+    print(f'Welcome')
+   
+
+login()
+
+   #inventory
+   # user = inventory.find('Name')
+    #user_password = inventory.find('Password')
