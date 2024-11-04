376f93c7520989f4474daa2a2059cbd900ff9deb
Katy Ashworth <katyashworth27@gmail.com>
2024-11-04 18:24:36 +0000

    Turn users' shopping list into a DataFrame > > Add a new column 'Quantity' of items ordered

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ from google.oauth2.service_account import Credentials
 
 
 import pandas as pd
-import numpy as np
 import re
 import datetime
+from collections import Counter 
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -22,8 +22,19 @@ SHEET = GSPREAD_CLIENT.open('good_food_network')
 inventory = SHEET.worksheet('inventory')
 customers = SHEET.worksheet('customers')
 
+#imports the data and changes to a data frame for easier viewing 
+inventory_li = inventory.get_all_records()
+inventory_df = pd.DataFrame(inventory_li)
 
-def match_and_return():
+#class Customer:
+    #def __init__(self, membership, search, bag, order, inventory_update):
+        #self.membership = membership
+        #self.search = stock
+       # self.bag = options
+        #self.order = selects
+    
+        
+def membership_m():
     """
     Matches the users input in a specific column and returns 
     * uses for dietary requirements
@@ -34,19 +45,19 @@ def match_and_return():
     https://medium.com/@vince.shields913/reading-google-sheets-into-a-pandas-dataframe-with-gspread-and-oauth2-375b932be7bf
     
     """
-
     name = customers.get_all_values()
     user_input = input("What is your name? ")
     
     for row in name:
         if row[0] == user_input:
             membership_no = row[1]
-            print(f"Welcome {user_input}, to Good Food Pantry Online. Your membership number {membership_no}\n")
+            print((f"Welcome {user_input}, to Good Food Pantry Online. Your membership number {membership_no}\n"))
+            return membership_no
             break
     else:
         print("We can't find your name in the list")
 
-def categories():
+def search():
 
     """
     Matches the users input in a specific column and returns 
@@ -58,48 +69,128 @@ def categories():
     https://shecancode.io/filter-a-pandas-dataframe-by-a-partial-string-or-pattern-in-8-ways/
     https://sentry.io/answers/write-a-list-to-a-file-in-python-with-each-item-on-a-new-line/
     """
- 
+
     user_search = input('Would you like to see only vegan or vegetarian foods? \n \n If yes, please write "Vegan" or "Vegetarian" below. \n \n ')
- 
-    filt = (df['Allegen'].str.contains(user_search, na=False)) & (df['Status'] == 'In stock') 
-    filt_df =df.loc[filt]
+
+    filt = (inventory_df['Allegen'].str.contains(user_search, na=False)) & (inventory_df['Status'] == 'In stock') 
+    filt_df =inventory_df.loc[filt]
 
     if not filt_df.empty:
         in_stock = filt_df[['Item_Name', 'Allegen', 'Status']]
-        #in_stock_item_index = in_stock.set_index('Item_Name')
         print(in_stock)
         return in_stock
     else:
-        print("Sorry, there are any items that match your request. Please try again")
+        print("Sorry, there are any items that match your request. Please try again:C")
 
-def selection(search_results):
+def bag(search_results):
 
     """
     User selects an item using the index and it is added to the shopping bag
     """
     shopping_bag = []
-    items = 1
-    while items < 6:
-        user_selects = int(input('\nChose an item:\n \n'))
+    items = 0
+
+    while items < 5:
+        user_selects = int(input('\nChose an item: '))
 
         try:
             in_the_bag = search_results.loc[user_selects, 'Item_Name']
             shopping_bag.append(in_the_bag)
             shopping_bag_str = '\n'.join(shopping_bag) 
             print(shopping_bag_str)
-            items += 1
+            items += 1  
 
         except (KeyError):
-            print("Sorry, we can't find that item. Please choose another item from the list")
+            print("Sorry, we can't find that item. Please choose another item from the list:KE")
 
         except (ValueError):
-            print("Sorry, we can't find that item. Please choose another item from the list")
+            print("Sorry, we can't find that item. Please choose another item from the list:VE")
 
         #BUG - ValueError when user doesn't input anything - programme restarts
 
-    
     print("Thank you, would you like to reserve your items or start again? \n")
-        
+    return shopping_bag_str
+
+def start_again():
+    pass
+
+def order_amount(shopping):
+    """
+    Calcuation the number of items the customer has in the shopping basket
+
+        for items in shopping_items:''
+        inventory_df.loc[inventory_df['Item_Name'] == items]
+        new = pd.join(inventory_df, shopping_items, on='Item_Name', how="inner")
+        print(new)
+    ref 
+    Flatten List
+    https://python.shiksha/tips/count-occurrences-of-a-list-item-in-python/
+    https://realpython.com/python-flatten-list/
+
+    Turn into dictionary DataFrame 
+    https://www.geeksforgeeks.org/how-to-create-dataframe-from-dictionary-in-python-pandas/
+
+    Inner Joing
+    https://www.kdnuggets.com/2023/03/3-ways-merge-pandas-dataframes.html
+
+
+    """
+    shopping_items = []
+    shopping_list = shopping.split('\n')
+    shopping_items.append(shopping_list)
+
+    flat_list = [item for new_list in shopping_items for item in new_list]
+
+    item_count = Counter(flat_list)
+    print(item_count)  
+
+    shopping_df = pd.DataFrame(list(item_count.items()))
+    #shopping_df = pd.DataFrame(item_count, index=shopping_items)
+
+    
+    shopping_df = shopping_df.rename(columns={0: 'Item_Name', 1: 'Quantity'})
+
+    print(shopping_df)
+
+
+    shopping_cart = inventory_df.merge(shopping_df, on='Item_Name', how='right')        
+
+def order():
+    """
+    Updates the order spreadsheet
+    """
+    
+    user_selects = inventory_df[['Item_Name', 'Stock']]
+    print(user_selects)
+
+
+def main():
+    #customer = Customer()
+    #membership = membership_m()
+    search_results = search()
+    shopping = bag(search_results)
+    #start_again()
+    order_amount(shopping) 
+
+    
+    #stock = in_stock()
+    
+    #shopping = bag(search_results) 
+    
+    #shopping_stock_calc(shopping)      
+    #expired_items = expired()
+
+main()
+
+
+# deduct selection from the inventory 
+# call number and minus one 
+# update speadsheet
+
+#Call user selection 
+#user_selects 
+
+# 
 
 # ---------------------------Manipulation of data of the gspread--------------------
 
@@ -114,10 +205,6 @@ def expired():
     today = datetime.date.today()
     today_pd = pd.to_datetime(today)
 
-    #imports the data and changes to a data frame for easier viewing 
-    inventory_li = inventory.get_all_records()
-    inventory_df = pd.DataFrame(inventory_li)
-
     #Changes the data type of the expiry_date column from string to Pandas format eg datetime64[ns] 
     inventory_df['Expiry_Date'] = pd.to_datetime(inventory_df['Expiry_Date'], format='%d/%m/%y')
 
@@ -126,11 +213,6 @@ def expired():
     expired_items = inventory_df["Expiry_Date"] < today_pd
 
     return expired_items
-
-
-
-expired_items = expired()
-
 
 
 def update_spreadsheet():
@@ -163,10 +245,6 @@ def sold_out():
     """
 
 
-
-
-
-
 def in_stock_spreadsheet():
     """
     Based on the expired and sold_out - show customers the available produce
@@ -177,16 +255,9 @@ def in_stock_spreadsheet():
         
 
 
+ 
     
-       
 
-    
-def main():
-    match_and_return()
-    search_results = categories()
-    selection(search_results)
-    
-#main()
 
   # set time and date
     # update status when something is out of stock 
@@ -233,37 +304,6 @@ def main():
 
     # Make those that fall in the subset expired_filt to change the status coloumn every subse't of inventory_df into   
     
-
-
-
-    """
-    #Takes todays date and changes it into Pandas format
-    today = datetime.date.today()
-    today_pd = pd.to_datetime(today)
-
-    #imports the data and changes to a data frame for easier viewing 
-    inventory_li = inventory.get_all_records()
-    inventory_df = pd.DataFrame(inventory_li)
-
-    #Changes the data type of the expiry_date column from string to Pandas format eg datetime64[ns]                 
-    inventory_df['Expiry_Date'] = pd.to_datetime(inventory_df['Expiry_Date'], format='%d/%m/%y')
-   
-    #creates a list of expired food
-    expired_items = inventory_df[inventory_df["Expiry_Date"] < today_pd]
-
-    #expired_filt = expired_status[['Item_Name', 'Expiry_Date']]   
-
-    #change the status column when food has expired 
-    
-    expired_items['Status'] = 'Expired'
-
-    inventory_df.update(expired_items) 
-      
-    #print(expired_filt)
-    print("Sucessfully updated")
-    print(expired_items)
-
-    """
 
         #filtered_df = inventory_df[inventory_df['Expiry_Date'].dt.strftime('%d/%m/%y') < today_obj]
     #
